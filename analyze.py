0426ddc54804250e30c83a2a2f8cbd8bce496058
amaechinwankwo <damaechi@rocketmail.com>
2026-02-01 18:15:27 +0100

    Converted workflow into functions

--- analyze.py
+++ analyze.py
@@ -1,9 +1,21 @@
-print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you give me!")
+#Welcome User
+def welcomeUser():
+    print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you give me!")
 
 #Get username
+def getUsername():
+    #Print a message, prompting user to input their username
+    usernameFromInput = username=input("\nTo begin, please enter your username\n")
+    return usernameFromInput;
 
-#Print a message, prompting user to input their username
-x=input("\nTo begin, please enter your username\n")
+#Great th User
+def greatUser(name):
+    print("Hello, " + name )
 
-#Greet the user
-print("Hello "+ x)
+def runProgram(): #calling functions in a function
+    welcomeUser();
+    username = getUsername()
+    greatUser(username)
+
+
+runProgram();
