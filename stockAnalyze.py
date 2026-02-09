959554ab878ba7217679a06bec36bf2ed1f48db5
amaechinwankwo <damaechi@rocketmail.com>
2026-02-09 20:58:58 +0100

    Got basic Stock Info

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -1,19 +1,61 @@
 import yfinance as yf
-
+from datetime import datetime  
 
 def extractBasicInfo(data):
-    keysToExtract = ['longName', 'website', 'fullTimeEmployees', 'marketCap', 'totalRevenue', 'trailingPE', 'sector']
-    basicInfo ={}
-    for key in keysToExtract:
-        if key in data:
-            basicInfo[key] = data[key]
-        else:
-            basicInfo[key] = ""
-    return basicInfo
+    keysToExtract = [
+        'longName', 'website', 'fullTimeEmployees',
+        'marketCap', 'totalRevenue', 'trailingPE', 'sector'
+    ]
+    return {key: data.get(key, "") for key in keysToExtract}
+
+def getPriceHistory(company):
+    historyDf = company.history(period='12mo')
+
+    return {
+        'price': historyDf['Close'].tolist(),
+        'date': historyDf.index.strftime('%Y-%m-%d').tolist()
+    }
+
+def getEarningsDate(company):
+    earningsDateDf = company.earnings_dates
+
+    if earningsDateDf is None or earningsDateDf.empty:
+        return []
+
+    currentDate = datetime.now()
+
+    futureDates = [
+        date.tz_localize(None).strftime('%Y-%m-%d')
+        for date in earningsDateDf.index
+        if date.tz_localize(None) > currentDate
+    ]
+    return futureDates
+
+def getCompanyNews(company):
+    allNewsArticles = []
+    for newsDict in company.news or []:
+        title = newsDict.get('title')
+        link = newsDict.get('link')
+
+        if not title or not link:
+            continue
+
+        allNewsArticles.append({
+            'title': title,
+            'link': link
+        })
+    return allNewsArticles
 
 def companyStockInfo(tickerSymbol):
-    company =yf.Ticker(tickerSymbol)
-    basicInfo =extractBasicInfo(company.info)
-    print(basicInfo)
 
-companyStockInfo("MSFT")
+    # Get data from Yahoo Finance
+    company = yf.Ticker(tickerSymbol)
+
+    # Get Basic info on Company
+    basicInfo = extractBasicInfo(company.info)
+    priceHistory = getPriceHistory(company)
+    futureEarningDates = getEarningsDate(company)
+    newsArticles = getCompanyNews(company)
+
+
+companyStockInfo('MSFT')
