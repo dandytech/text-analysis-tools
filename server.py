2d80ec60a68027342217dc89ef8ba1f343f2c2ad
amaechinwankwo <damaechi@rocketmail.com>
2026-02-10 19:07:54 +0100

    Added URL parameter toanalyze-stock get request

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Health Check'
 
-@app.route('/analyze-stock')
-def analyzeStock():
+@app.route('/analyze-stock/<ticker>')
+def analyzeStock(ticker):
     return  {
-        'data': 'Analysis coming soon'
+        'data': 'Analysis for ' + ticker + ' coming soon'
     }
 
 if __name__ == '__main__':
