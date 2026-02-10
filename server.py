2a82931ec8a59e3d5f72a55894be03f8476e2a8a
amaechinwankwo <damaechi@rocketmail.com>
2026-02-10 19:53:31 +0100

    Integrated Stock Analyze Code into analyze route handler

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
-from flask import Flask, abort
+from flask import Flask, abort, jsonify
+from stockAnalyze import getCompanyStockInfo
 
 app = Flask(__name__)
 
 @app.route('/health')
-def hello_world():
+def health():
     return 'Health Check'
 
 @app.route('/analyze-stock/<ticker>')
 def analyzeStock(ticker):
-    if len(ticker) > 5 or not ticker.isidentifier():
-        abort(400, 'Invalid ticker symbol')
-        return {'data': 'Analysis for ' + ticker + ' coming soon'}
-    
-# The main server function
+    # Validate ticker
+    if len(ticker) > 5 or not ticker.isalpha():
+        abort(400, description='Invalid ticker symbol')
+
+    # Get analysis
+    analysis = getCompanyStockInfo(ticker)
+
+    # ALWAYS return something
+    return jsonify(analysis)
+
+# Main server
 if __name__ == '__main__':
     app.run()
