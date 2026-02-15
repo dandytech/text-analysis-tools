8e0d0b63dfca5eef7f853823d6bcb7ad088e744d
amaechinwankwo <damaechi@rocketmail.com>
2026-02-15 13:48:20 +0100

    Prepared server for render deployment

--- server.py
+++ server.py
@@ -13,23 +13,23 @@ CORS(app)
 
 @app.route('/health', methods=["GET"])
 def health():
-    return 'Health Check'
+    return 'Flask Server is Running'
 
 @app.route('/analyze-stock/<ticker>', methods=["GET"])
 def analyzeStock(ticker):
     #return stockDataTest ## comment out this line to make it live
 
    # Validate ticker
-    if len(ticker) > 5 or not ticker.isalpha():
-        abort(400, description='Invalid ticker symbol')
+    # if len(ticker) > 5 or not ticker.isalpha():
+    #     abort(400, description='Invalid ticker symbol')
 
-    try:
+    # try:
         # Get analysis
-        analysis = getCompanyStockInfo(ticker)
-    except NameError as e:
-        abort(404, e)
-    except:
-         abort(500, description='Something Went Wrong runing the stock analysis.')
+    analysis = getCompanyStockInfo(ticker)
+    # except NameError as e:
+    #     abort(404, e)
+    # except:
+    #      abort(500, description='Something Went Wrong runing the stock analysis.')
     # ALWAYS return something
     return jsonify(analysis)
 
